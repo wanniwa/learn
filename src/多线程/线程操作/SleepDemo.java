2ec75fa1a8315b23f6592f407ad867cbb7adbcf7
王宁 <734933034@qq.com>
2018-09-27 09:22:05 +0800

    sleep

--- src/多线程/线程操作/SleepDemo.java
+++ src/多线程/线程操作/SleepDemo.java
@@ -2,10 +2,11 @@ package 多线程.线程操作;
 
 public class SleepDemo extends Thread {
     public static void main(String[] args) throws InterruptedException {
-        SleepDemo joinDemo01 = new SleepDemo();
-        joinDemo01.start();
+        SleepDemo sleepDemo = new SleepDemo();
+        sleepDemo.start();
         for (int i = 0; i < 10; i++) {
             if (i == 5) {
+                System.out.println("sleep 5s");
                 sleep(10000);
             }
             System.out.println("main"+i);
@@ -14,7 +15,7 @@ public class SleepDemo extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Join"+i);
+            System.out.println("sleep"+i);
         }
     }
 }
