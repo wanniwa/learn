08c1a552e69dc22ab341aa94f0127028f6a276f9
wanniwa <734933034@qq.com>
2018-09-29 23:14:19 +0800

    多线程

--- src/多线程/方法三Callable/Call.java
+++ src/多线程/方法三Callable/Call.java
@@ -2,42 +2,59 @@ package 多线程.方法三Callable;
 
 import java.util.concurrent.*;
 
+/**
+ * 使用Callable创建线程
+ * 1）、创建Callable实现类+重写call
+ * 2）、借助调度服务ExecutorService，获得Future对象
+ * ExecutorService ser= Executors.newFixedThreadPool(2);
+ * Future result = ser.submit(实现类对象)
+ * 3）、获取值 result.get()
+ * 4）、停止服务ser.shutdownNow()
+ */
 public class Call {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         //创建一个可重用固定线程数的线程池
         ExecutorService ser = Executors.newFixedThreadPool(2);
-        Race tortoise = new Race("千年王八",1000L);
-        Race rabbit = new Race("兔子",500L);
-        //FutureTask 也可以了解一下
+        ExecutorService ser2 = Executors.newCachedThreadPool();
+        Race tortoise = new Race("千年王八", 1000L);
+        Race rabbit = new Race("兔子", 500L);
+        Future<Integer> result3 = ser2.submit(tortoise);
         Future<Integer> result = ser.submit(tortoise);
         Future<Integer> result2 = ser.submit(rabbit);
+
         Thread.sleep(2000);
         tortoise.setFlag(false);
         rabbit.setFlag(false);
+
         int num1 = result.get();
         int num2 = result2.get();
-        System.out.println("乌龟跑了————>"+num1);
-        System.out.println("兔子跑了————>"+num2);
+        System.out.println("乌龟跑了————>" + num1);
+        System.out.println("兔子跑了————>" + num2);
         ser.shutdownNow();
     }
 }
+
 class Race implements Callable<Integer> {
     private String name;//名称
     private long time;//延时时间
     private boolean flag = true;
     private int step = 0;
+
     public Race() {
     }
+
     public Race(String name) {
         this.name = name;
     }
+
     public Race(String name, long time) {
         this.name = name;
         this.time = time;
     }
+
     @Override
     public Integer call() throws Exception {
-        while (flag){
+        while (flag) {
             Thread.sleep(time);
             step++;
         }
@@ -75,5 +92,4 @@ class Race implements Callable<Integer> {
     public void setStep(int step) {
         this.step = step;
     }
-
 }
