6553ab4f54b95fcbffb1bf480708002cbb058bff
wangning1 <wangning1@yunzhangfang.com>
2021-04-02 22:35:24 +0800

    算法

--- src/main/java/算法/排序/冒泡排序.java
+++ src/main/java/算法/排序/冒泡排序.java
@@ -1,7 +1,118 @@
 package 算法.排序;
 
+import java.util.Arrays;
+
 public class 冒泡排序 {
     public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 0};
+        sort4(arr);
 
+    }
+
+    private static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    private static void sort1(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swagFlag = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swagFlag = true;
+                }
+            }
+            if (!swagFlag) {
+                return;
+            }
+        }
+    }
+
+    private static void sort2(int[] arr) {
+        int left = arr.length - 1;
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swagFlag = false;
+            int pos = 0;
+            for (int j = 0; j < left; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swagFlag = true;
+                    pos = j;
+                }
+            }
+            if (!swagFlag) {
+                return;
+            }
+            left = pos;
+        }
+    }
+
+    private static void sort3(int[] arr) {
+        int left = arr.length - 1;
+        for (int i = 0; i < arr.length - 1 & left > 0; i++) {
+            int pos = 0;
+            for (int j = 0; j < left; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    pos = j;
+                }
+            }
+            left = pos;
+        }
+    }
+
+    private static void sort4(int[] arr) {
+        int right = arr.length - 1;
+        int left = 0;
+        while (left < right) {
+            boolean swagFlag = false;
+            int pos = 0;
+            for (int j = left; j < right; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swagFlag = true;
+                    pos = j;
+                }
+            }
+            right = pos;
+            if (!swagFlag) {
+                return;
+            }
+            swagFlag = false;
+            int pos1 = 0;
+            for (int k = right; k > left; k--) {
+                if (arr[k] < arr[k - 1]) {
+                    int tmp = arr[k];
+                    arr[k] = arr[k - 1];
+                    arr[k - 1] = tmp;
+                    swagFlag = true;
+                    pos1 = k;
+                }
+            }
+            left = pos1;
+            if (!swagFlag) {
+                return;
+            }
+        }
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.println(Arrays.toString(arr));
     }
 }
---
+++ src/main/java/算法/排序/插入排序.java
@@ -0,0 +1,35 @@
+package 算法.排序;
+
+import java.util.Arrays;
+
+public class 插入排序 {
+    public static void main(String[] args) {
+        int[] arr = {7, 10, 12, 3, 9, 5};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void sort(int[] arr) {
+
+        // 从下标为1的元素开始选择合适的位置插入，因为下标为0的只有一个元素，默认是有序的
+        for (int i = 1; i < arr.length; i++) {
+
+            // 记录要插入的数据
+            int tmp = arr[i];
+
+            // 从已经排序的序列最右边的开始比较，找到比其小的数
+            int j = i;
+            while (j > 0 && tmp < arr[j - 1]) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+
+            // 存在比其小的数，插入
+            if (j != i) {
+                arr[j] = tmp;
+            }
+
+        }
+    }
+}
